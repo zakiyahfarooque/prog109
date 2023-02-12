544262415071f1b4ba2fed41f24cf909c1879e7c
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-11 20:31:19 -0800

    Update artifact.js

--- Artifact3/artifact.js
+++ Artifact3/artifact.js
@@ -33,6 +33,53 @@ rLine +="</p>";
 document.getElementById("upRight").innerHTML = rLine;
 }
 
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<=pHeight-i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upRight").innerHTML = rLine;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
