10c032b0c3f963953b80fd3411f3f08783dc64d8
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-11 20:18:03 -0800

    Update artifact.js

--- Artifact3/artifact.js
+++ Artifact3/artifact.js
@@ -4,8 +4,8 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upSide(pHeight, pColorEven, pColorOdd, pSymbol);
-
+upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
@@ -30,34 +30,9 @@ rLine +="</p>";
 
 }
 
-document.getElementById("upSide").innerHTML = rLine;
+document.getElementById("upRight").innerHTML = rLine;
 }
-function upSide(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (var i=0;i<=pHeight;i++){
-    for (var j=0; j<=i+1;j++){
-    rLine +="<p>";
-//Create each line on the Rhombus
-//Is the position even or odd so we change the color
-        if (j%2){
-//even
-        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-        else
-//odd
-        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 
-            }
-        rLine +="</p>";
-// console.log(rLine);
-
-}
-document.getElementById("upSide").innerHTML = rLine;
-}
-}
-    
-    
-    
-    
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=pHeight;i > 0;i--){
@@ -82,28 +57,4 @@ rLine +="</p>";
 document.getElementById("downRight").innerHTML = rLine;
 }
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-var rLine ="";
-for (i=0;i<pHeight;i++){
-    for(j=1;j<=i;j++){
-       rLine +="<p>";
-    }
-//Create each line on the Rhombus
-    for(j=i;j<=i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("downLeft").innerHTML = rLine;
-}
+ 
