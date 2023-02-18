830c12276989befcf9aeab125776e1cf1d239d48
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-17 23:22:21 -0800

    Update addElement.js

--- Module08/addElement.js
+++ Module08/addElement.js
@@ -1,8 +1,10 @@
+var newItem = document.getElementById("items").value;
+
 // Create a new element and store it in a variable.
 var newEl = document.createElement('li');
 
 // Create a text node and store it in a variable.
-var newText = document.createTextNode('quinoa');
+var newText = document.createTextNode(newItem);
 
 // Attach the new text node to the new element.
 newEl.appendChild(newText);
