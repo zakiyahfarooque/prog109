0fc579e078adf975d1cf264b5e9480ecb33ff944
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-20 22:52:53 -0800

    Update artifact.js

--- Artifact3/artifact.js
+++ Artifact3/artifact.js
@@ -1,26 +1,46 @@
 function makeRhombus() {
-	let size = parseInt(document.getElementById("size").value);
-	let color = document.getElementById("color").value;
-	let symbol = document.getElementById("symbol").value;
+  let size = parseInt(document.getElementById("size").value);
+  let symbol = document.getElementById("symbol").value;
+  let color1 = document.getElementById("color1").value;
+  let color2 = document.getElementById("color2").value;
 
-	let rhombus = "";
-	for (let i = 0; i < size; i++) {
-		for (let j = 0; j < size - i - 1; j++) {
-			rhombus += "&nbsp;&nbsp;";
-		}
-		for (let j = 0; j < 2 * i + 1; j++) {
-			rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
-		}
-		rhombus += "<br>";
-	}
-	for (let i = size - 2; i >= 0; i--) {
-		for (let j = 0; j < size - i - 1; j++) {
-			rhombus += "&nbsp;&nbsp;";
-		}
-		for (let j = 0; j < 2 * i + 1; j++) {
-			rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
-		}
-		rhombus += "<br>";
-	}
-	document.getElementById("rhombus").innerHTML = rhombus;
+  let rhombus = "";
+  let colors = [color1, color2];
+  let colorIndex = 0;
+
+  for (let i = 0; i < size; i++) {
+    for (let j = size - i; j > 1; j--) {
+      rhombus += "&nbsp;&nbsp;";
+    }
+    for (let j = 0; j <= i; j++) {
+      let color = colors[colorIndex % 2];
+      rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+      colorIndex++;
+    }
+    for (let j = i - 1; j >= 0; j--) {
+      let color = colors[colorIndex % 2];
+      rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+      colorIndex++;
+    }
+    rhombus += "<br>";
+  }
+
+  for (let i = size - 2; i >= 0; i--) {
+    for (let j = size - i; j > 1; j--) {
+      rhombus += "&nbsp;&nbsp;";
+    }
+    for (let j = 0; j <= i; j++) {
+      let color = colors[colorIndex % 2];
+      rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+      colorIndex++;
+    }
+    for (let j = i - 1; j >= 0; j--) {
+      let color = colors[colorIndex % 2];
+      rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+      colorIndex++;
+    }
+    rhombus += "<br>";
+  }
+
+  document.getElementById("rhombus").innerHTML = rhombus;
 }
