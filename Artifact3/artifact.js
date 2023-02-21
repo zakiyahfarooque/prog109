501fae708b503d3e44cbfff79e0f568cdaefff90
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-20 19:58:49 -0800

    Update artifact.js

--- Artifact3/artifact.js
+++ Artifact3/artifact.js
@@ -1,73 +1,26 @@
-// var rHeight =5;
-// var colorEven = "orange";
-// var colorOdd = "black";
-// var symbol ="*";
+function makeRhombus() {
+	let size = parseInt(document.getElementById("size").value);
+	let color = document.getElementById("color").value;
+	let symbol = document.getElementById("symbol").value;
 
-function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-upSide(pHeight, pColorEven, pColorOdd, pSymbol);
-downSide(pHeight, pColorEven, pColorOdd, pSymbol);
-
-
+	let rhombus = "";
+	for (let i = 0; i < size; i++) {
+		for (let j = 0; j < size - i - 1; j++) {
+			rhombus += "&nbsp;&nbsp;";
+		}
+		for (let j = 0; j < 2 * i + 1; j++) {
+			rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+		}
+		rhombus += "<br>";
+	}
+	for (let i = size - 2; i >= 0; i--) {
+		for (let j = 0; j < size - i - 1; j++) {
+			rhombus += "&nbsp;&nbsp;";
+		}
+		for (let j = 0; j < 2 * i + 1; j++) {
+			rhombus += "<span style=\"color: " + color + "\">" + symbol + "</span>";
+		}
+		rhombus += "<br>";
+	}
+	document.getElementById("rhombus").innerHTML = rhombus;
 }
-
-function upSide(pHeight, pColorEven, pColorOdd, pSymbol) {
-
- for (i = 1; i <= pHeight; i++) {
-  var rLine =" ";
-  
- 
-for(j=1; j<=pHeight - i; j++) {  
- rLine +="<p>";
-}
- 
-for (k=1; k<=2*i - 1; k++) {
-//Create each line on the Rhombus
-
-
-//Is the position even or odd so we change the color
-if (k%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-document.getElementById("upSide").innerHTML = rLine;
-
-}
-
-
-function downSide(pHeight, pColorEven, pColorOdd, pSymbol){
-
-for (i= pHeight - 1;i >= 1;i--){
-	var rLine =" ";
-//Create each line on the Rhombus
-for( j=1; j<= pHeight - i;j++){
- rLine += "<p>";
-}
-for ( k =1; k<=2*i-1; k++){
-//Is the position even or odd so we change the color
-if (k%2)
-//even
-rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-else
-//odd
-rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-}
-rLine +="</p>";
-// console.log(rLine);
-
-}
-
-  document.getElementById("downSide").innerHTML = rLine;
-
-}
-
-
