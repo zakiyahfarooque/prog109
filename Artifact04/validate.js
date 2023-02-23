ae688a5b05a139c1441173a581245c17dbb389a5
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-23 11:35:17 -0800

    Update validate.js

--- Artifact04/validate.js
+++ Artifact04/validate.js
@@ -24,48 +24,16 @@ function validateForm() {
   var nameRegex = /^[a-zA-Z]{1,20}$/;
   if (!nameRegex.test(name)) {
     errorMessages.push("Name should only contain alphabetical characters and be between 1 and 20 characters long.");
+    document.getElementById("nameError").innerHTML = "Name should only contain alphabetical characters and be between 1 and 20 characters long.";
+    document.getElementById("nameError").classList.add("error");
   }
 
   // validate email
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     errorMessages.push("Email is not valid.");
+    document.getElementById("emailError").innerHTML = "Email is not valid.";
+    document.getElementById("emailError").classList.add("error");
   }
 
-  // validate phone
-  var phoneRegex = /^[0-9-]+$/;
-  if (!phoneRegex.test(phone)) {
-    errorMessages.push("Phone number should only contain numbers or numbers and dashes.");
-  }
-
-  // validate username
-  if (username.length < 3) {
-    errorMessages.push("Username should be at least 3 characters long.");
-  }
-
-  // validate password
-  var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{1,7}$/;
-  if (!passwordRegex.test(password)) {
-    errorMessages.push("Password should be between 1 and 7 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (@#$%^&+=!).");
-  }
-
-  // validate zipcode
-  if (country.toLowerCase() === "usa" && zipcode.length !== 5) {
-    errorMessages.push("Zipcode should be 5 digits long.");
-  }
-
-  // display all error messages if any validations fail
-  if (errorMessages.length > 0) {
-    errorMessages.forEach(function(errorMessage) {
-      var errorElement = document.createElement("div");
-      errorElement.classList.add("error");
-      errorElement.innerHTML = errorMessage;
-      document.getElementById("formErrors").appendChild(errorElement);
-    });
-
-    return false;
-  }
-
-  // return true if all validations pass
-  return true;
-}
+  //
