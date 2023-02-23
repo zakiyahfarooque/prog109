9b9f246ab2a37f1ebe88a7a0af752badcfc4cc7d
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-23 11:13:14 -0800

    Update validate.js

--- Artifact04/validate.js
+++ Artifact04/validate.js
@@ -1,34 +1,62 @@
-// Initialize form validator
-var frmvalidator = new Validator("myForm");
+function validateForm() {
+  // get form input values
+  var name = document.getElementById("name").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var phone = document.getElementById("phone").value.trim();
+  var username = document.getElementById("username").value.trim();
+  var password = document.getElementById("password").value.trim();
+  var state = document.getElementById("state").value.trim();
+  var country = document.getElementById("country").value.trim();
+  var zipcode = document.getElementById("zipcode").value.trim();
 
-// Add validation rules for form inputs
-frmvalidator.addValidation("name","req","Please enter your name.");
-frmvalidator.addValidation("name","alpha_s","Name must only contain alphabetical characters.");
-frmvalidator.addValidation("name","maxlen=20","Name must be no longer than 20 characters.");
+  // clear error messages
+  document.getElementById("nameError").innerHTML = "";
+  document.getElementById("emailError").innerHTML = "";
+  document.getElementById("phoneError").innerHTML = "";
+  document.getElementById("usernameError").innerHTML = "";
+  document.getElementById("passwordError").innerHTML = "";
+  document.getElementById("zipcodeError").innerHTML = "";
 
-frmvalidator.addValidation("email","req","Please enter your email address.");
-frmvalidator.addValidation("email","email","Please enter a valid email address.");
+  // validate name
+  var nameRegex = /^[a-zA-Z]{1,20}$/;
+  if (!nameRegex.test(name)) {
+    document.getElementById("nameError").innerHTML = "Name should only contain alphabetical characters and be between 1 and 20 characters long.";
+    return false;
+  }
 
-frmvalidator.addValidation("phone","req","Please enter your phone number.");
-frmvalidator.addValidation("phone","numdash","Phone must only contain numerical characters and dashes.");
+  // validate email
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    document.getElementById("emailError").innerHTML = "Email is not valid.";
+    return false;
+  }
 
-frmvalidator.addValidation("username","req","Please enter a username.");
+  // validate phone
+  var phoneRegex = /^[0-9-]+$/;
+  if (!phoneRegex.test(phone)) {
+    document.getElementById("phoneError").innerHTML = "Phone number should only contain numbers or numbers and dashes.";
+    return false;
+  }
 
-frmvalidator.addValidation("password","req","Please enter a password.");
-frmvalidator.addValidation("password","maxlen=7","Password must be no longer than 7 characters.");
-frmvalidator.addValidation("password","regexp=^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};:\\'\\\"<>,./?\\\\]).*$","Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
+  // validate username
+  if (username.length < 3) {
+    document.getElementById("usernameError").innerHTML = "Username should be at least 3 characters long.";
+    return false;
+  }
 
-frmvalidator.addValidation("state","req","Please enter your state/province.");
+  // validate password
+  var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{1,7}$/;
+  if (!passwordRegex.test(password)) {
+    document.getElementById("passwordError").innerHTML = "Password should be between 1 and 7 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (@#$%^&+=!).";
+    return false;
+  }
 
-frmvalidator.addValidation("country","dontselect=0","Please select a country.");
-frmvalidator.addValidation("zipcode","req","Please enter your zipcode.");
-frmvalidator.addValidation("zipcode","num","Zipcode must only contain numerical characters.");
-frmvalidator.addValidation("zipcode","maxlen=5","Zipcode must be no longer than 5 characters.");
+  // validate zipcode
+  if (country.toLowerCase() === "usa" && zipcode.length !== 5) {
+    document.getElementById("zipcodeError").innerHTML = "Zipcode should be 5 digits long.";
+    return false;
+  }
 
-// Set error message display location
-frmvalidator.setAddnlValidationFunction(showErrors);
-frmvalidator.setFormFocus(true);
-
-// Function to show error messages
-function showErrors() {
-  var errors = frmvalidator._error
+  // return true if all validations pass
+  return true;
+}
