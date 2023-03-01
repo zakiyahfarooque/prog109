985e5fbc9a89793dd2e3752a4186f7e63c1bb0d8
zakiyahfarooque <122185308+zakiyahfarooque@users.noreply.github.com>
2023-02-28 23:47:43 -0800

    Update slideshow.js

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,37 +1,66 @@
-var myImages =["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg","http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
+// Array of image URLs and descriptions
+const images = [
+	{
+		url: 'images/image1.jpg',
+		description: 'Description 1'
+	},
+	{
+		url: 'images/image2.jpg',
+		description: 'Description 2'
+	},
+	{
+		url: 'images/image3.jpg',
+		description: 'Description 3'
+	},
+	{
+		url: 'images/image4.jpg',
+		description: 'Description 4'
+	},
+	{
+		url: 'images/image5.jpg',
+		description: 'Description 5'
+	}
+];
 
+// Get HTML elements
+const imageEl = document.getElementById('slideshow-image');
+const descriptionEl = document.getElementById('slideshow-description');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
 
-var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+// Index of current image
+let currentImageIndex = 0;
 
- var index=0; 
+// Show the current image and description
+function showImage() {
+	imageEl.src = images[currentImageIndex].url;
+	descriptionEl.textContent = images[currentImageIndex].description;
+}
 
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= captionImages[index];
- document.getElementById("caption").textContent = captionImages[index]; 
-} 
+// Handle user clicks on Previous button
+function onPrevClick() {
+	currentImageIndex--;
+	if (currentImageIndex < 0) {
+		currentImageIndex = images.length - 1;
+	}
+	showImage();
+}
 
-function next(){
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
- 
+// Handle user clicks on Next button
+function onNextClick() {
+	currentImageIndex++;
+	if (currentImageIndex >= images.length) {
+		currentImageIndex = 0;
+	}
+	showImage();
+}
 
-function back(){
- if (index===0)
- index=myImages.length-1;
- else
- index--;
- 
- updateImage();
-} 
+// Add event listeners to buttons
+prevBtn.addEventListener('click', onPrevClick);
+nextBtn.addEventListener('click', onNextClick);
 
-var nextButton = document.getElementById("next"); 
-var previousButton = document.getElementById("previous"); 
+// Show the first image
+showImage();
 
-previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
-
+// Automatically show the next image every 3 seconds
+setInterval(onNextClick, 3000);
